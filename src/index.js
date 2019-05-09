92bc2947d538072e7a916e3c466258962d901e9f
DESKTOP-STL2SEP\desss <sadovnikr@gmail.com>
2019-05-09 14:57:54 +0300

    first stap

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+fetch('http://swapi.co/api/planets/1/')
+    .then((res)=>{
+        console.log('Got response ', res.status);
+    });
